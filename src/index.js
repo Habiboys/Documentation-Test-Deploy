9c869ee9def1b23c64b23d6df54701ee5a16b768
Habiboys <nouvalhabibie18@gmail.com>
2025-05-10 18:23:21 +0700

    config db

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ const db = require('./models');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
-
+const cors = require('cors');
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 db.sequelize.sync();
 
 app.use(express.json());
