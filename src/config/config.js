53ea5004b52da15ca209e9292fa03f235488c13d
Habiboys <nouvalhabibie18@gmail.com>
2025-05-10 18:08:41 +0700

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config("../");
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306
+  },
+  test: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: 'database_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: process.env.DB_DIALECT || 'mysql'
+  },
+  production: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: 'database_production',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: process.env.DB_DIALECT || 'mysql'
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const indexRouter = require('./routes/index');
+const app = express();
+const port = 3000;
+const db = require('./models');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+db.sequelize.sync();
+
+app.use(express.json());
+app.use('/api', indexRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
+})
